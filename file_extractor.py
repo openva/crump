8c77c2c1d339a1ece803e032fe8952907e656228
Brian J. Geiger <bgeiger@pobox.com>
2014-05-04 16:35:50 -0400

    Got partway through fixing #23, though I think something a bit better than "replace all problem characters with spaces" will be called for. I also started breaking out the code into functions, so we can put in some better structure as we get everything working well.

--- file_extractor.py
+++ file_extractor.py
@@ -11,61 +11,76 @@ output_dir = "output"
 
 # Requires PyYAML <http://pyyaml.org/>
 import yaml
-import csv
+import csvkit
 import os
 import errno
 import glob
 
-# Create the output directory.
-try:
-    os.makedirs(output_dir)
-except OSError as exception:
-    if exception.errno != errno.EEXIST:
-        raise
 
-field_maps = {}
+def main():
+    # Create the output directory.
+    try:
+        os.makedirs(output_dir)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
 
-for the_file in glob.glob("table_maps/*.yaml"):
-    stream = open(the_file, 'r')
+    field_maps = {}
 
-    # Import the data from YAML.
-    field_map = yaml.load(stream)
+    for the_file in glob.glob("table_maps/*.yaml"):
+        stream = open(the_file, 'r')
 
-    head, tail = os.path.split(the_file)
+        # Import the data from YAML.
+        field_map = yaml.load(stream)
 
-    map_id = tail[0]
-    field_maps[map_id] = {}
-    field_maps[map_id]["map"] = field_map
-    field_maps[map_id]["name"] = tail
+        head, tail = os.path.split(the_file)
 
-# Count the number of lines in the master file.
-last_file = ""
-current_map = None
-csv_writer = None
-with open(master_file) as f:
-    for current_line in enumerate(f):
-        file_number = current_line[1][1]
-        if file_number in field_maps:
-            if file_number != last_file:
-                # new file - grab proper field map and do file setup
-                current_map = field_maps[file_number]["map"]
-                current_name = field_maps[file_number]["name"]
-                csv_name = current_name.replace(".yaml", ".csv")
-                last_file = file_number
-                the_file = open("output/"+csv_name, 'wb')
-                field_names = []
+        map_id = tail[0]
+        field_maps[map_id] = {}
+        field_maps[map_id]["map"] = field_map
+        field_maps[map_id]["name"] = tail
+
+    # Count the number of lines in the master file.
+    last_file = ""
+    current_map = None
+    csv_writer = None
+    with open(master_file) as f:
+        for current_line in enumerate(f):
+            file_number = current_line[1][1]
+            if file_number in field_maps:
+                if file_number != last_file:
+                    # new file - grab proper field map and do file setup
+                    current_map = field_maps[file_number]["map"]
+                    current_name = field_maps[file_number]["name"]
+                    csv_name = current_name.replace(".yaml", ".csv")
+                    last_file = file_number
+                    the_file = open("output/"+csv_name, 'wb')
+                    field_names = []
+                    for field in current_map:
+                        field_names.append(field["name"])
+                    field_tuple = tuple(field for field in field_names)
+                    csv_writer = csvkit.DictWriter(the_file, field_tuple)
+                    csv_writer.writeheader()
+                    print "Creating",csv_name
+                # break the line out into pieces
+                line = {}
                 for field in current_map:
-                    field_names.append(field["name"])
-                field_tuple = tuple(field for field in field_names)
-                csv_writer = csv.DictWriter(the_file, fieldnames=field_tuple)
-                csv_writer.writeheader()
-                print "Creating",csv_name
-            # break the line out into pieces
-            line = {}
-            for field in current_map:
-                start = int(field["start"])
-                length = int(field["length"])
-                name = field["name"]
-                end = start + length
-                line[name] = current_line[1][start:end].strip()
-            csv_writer.writerow(line)
+                    start = int(field["start"])
+                    length = int(field["length"])
+                    name = field["name"]
+                    end = start + length
+                    line[name] = current_line[1][start:end].strip()
+                try:
+                    csv_writer.writerow(line)
+                except UnicodeDecodeError as exception:
+                    print "Found an incorrect character in",line,"and hopefully fixed it."
+                    for key in line:
+                        line[key] = remove_non_ascii(line[key])
+                    csv_writer.writerow(line)
+
+# From http://stackoverflow.com/a/1342373/3579517 with slight modification to replace with space
+def remove_non_ascii(s):
+    return "".join(i if ord(i)<128 else " " for i in s )
+
+if __name__ == "__main__":
+    main()
